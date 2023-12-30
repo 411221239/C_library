41d2a42a6e7b3de9e858283f946c55bb5093d6b7
411221239 <147474157+411221239@users.noreply.github.com>
2023-12-30 17:25:23 +0900

    Create log.c

---
+++ math.h/log.c
@@ -0,0 +1,20 @@
+/* 用法 double log(double x) 
+
+  x -- 是浮點數值。 */
+
+//範例
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+   double x, ret;
+
+   x = 2.7; //預計計算的真數
+   ret = cos(x*val);
+   printf("log( %lf ) = %lf", x, ret);
+   
+   return(0);
+}
+
+//結果
+//log( 2.7 ) = 0.993252
