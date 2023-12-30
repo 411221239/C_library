c40235f474d094f95401a8099ce2a09bf8311ef1
411221239 <147474157+411221239@users.noreply.github.com>
2023-12-30 17:19:13 +0900

    Create tan.c

---
+++ math.h/tan.c
@@ -0,0 +1,23 @@
+/* 用法 double tan(double x) 
+
+  x -- 這是表示以弧度表示的角度的浮點數值。 */
+
+//範例
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159265
+
+int main () {
+   double x, ret, val;
+
+   x = 30.0; //預計計算的角度
+   val = PI / 180; //轉成弧度的常數
+   ret = tan(x*val);
+   printf("%lf 度的正切為 %lf", x, ret);
+   
+   return(0);
+}
+
+//結果
+//30.000000 度的正切為 0.577350
