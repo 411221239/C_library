85e1b77195085c91226a41c998803156bb0deb3b
411221239 <147474157+411221239@users.noreply.github.com>
2024-01-07 15:10:49 +0900

    Create abort.c

---
+++ stdlib.h/abort.c
@@ -0,0 +1,33 @@
+/*
+  C庫函數void abort(void)中止程序執行並直接退出呼叫的地方。
+
+  用法：void abort(void);
+
+  返回值：該函數不返回任何值。
+*/
+
+// 範例
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+   FILE *fp;
+   
+   printf("嘗試打開 nofile.txt\n");
+   fp = fopen( "nofile.txt","r" );
+   if(fp == NULL) {
+      printf("即將中止程序\n");
+      abort();
+   }
+   printf("即將關閉 nofile.txt\n");
+   fclose(fp);
+   
+   return(0);
+}
+// 編譯並運行上面的程序將在嘗試打開不存在的 nofile.txt 文件時產生以下結果 -
+// 結果
+/*
+嘗試打開 nofile.txt                                                    
+即將中止程序                                                  
+Aborted (core dumped)
+*/
